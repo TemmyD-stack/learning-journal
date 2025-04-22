5825a1a0bce11365d96e7b7724df8423460281ed
TemmyD-stack <temi.dire.dayo.1@gmail.com>
2025-04-22 16:29:09 +0100

    added menu bars for mobile responsiveness

--- index.js
+++ index.js
@@ -13,22 +13,14 @@ let date = new Date();
 const options = { year: 'numeric', month: 'long', day: 'numeric' };
 dateEl.innerText = date.toLocaleDateString('en-US', options);
 
-// console.log(data)
-
-// data.forEach((blog) => {
-//     const blogEl = document.createElement("div");
-//     blogEl.classList.add("blog-card");
-//     blogEl.innerHTML += `
-    // <div class="blog-card">
-//         <img src="${blog.image}" alt="Blog Image" class="blog-image">
-//         <div class="blog-content">
-//             <p class="blog-date">${blog.date}</p>
-//             <h2 class="blog-title">${blog.title}</h2>   
-//             <p class="blog-body">${blog.body}</p>
-//         </div>
-//   </div>`;
-//     blogContainer.appendChild(blogEl);
-// }) 
+// hamburger is here
+const hamburger = document.getElementById("menu-toggle");
+hamburger.addEventListener("click", () => {
+    const nav = document.getElementById("nav-links");
+    nav.classList.toggle('show');
+    
+})
+// blog posts are here 
 data.forEach((blog) => {
     if(blog.id > 6) return; // Limit to 6 blogs
     blogContainer.innerHTML += 
